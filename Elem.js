325fccab6c134f6215fad0de327185242d2b08be
SzluDora <szlucska.dora@gmail.com>
2023-10-07 23:20:32 +0200

    +Elem.js, stílus, menü

---
+++ Elem.js
@@ -0,0 +1,9 @@
+class Elem{
+	constructor(){
+		
+	}
+
+	// Készíts egy Elem osztályt az Elem.js fájlban, mely létrehoz egyetlen div-et,
+	// abban egy p taget. A ccs-ben állíts keretet és szélességet, magasságot a divnek.
+	//  Ha rákattintunk a div-re, írjon a p tagbe gey X-et. 
+}
